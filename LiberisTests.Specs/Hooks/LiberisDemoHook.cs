feb7aa06b66920778ab8ab458bf587356c28f539
Ifritzz <alejandro.bautista89@outlook.com>
2021-11-15 15:29:13 +0000

    Code refactor to fix dependencies

---
+++ LiberisTests.Specs/Hooks/LiberisDemoHook.cs
@@ -0,0 +1,17 @@
+﻿using LiberisTests.Specs.Drivers;
+using LiberisTests.Specs.PageObject;
+using TechTalk.SpecFlow;
+
+namespace LiberisTests.Specs.Hooks
+{
+    [Binding]
+    public sealed class LiberisDemoHook
+    {
+        [BeforeScenario("GetDemo")]
+        public void BeforeScenario(BrowserDriver browserDriver)
+        {
+            var liberisPageObject = new LiberisPageObject(browserDriver.Current);
+            liberisPageObject.OpenBrowserIfClosed();
+        }
+    }
+}
---
+++ LiberisTests.Specs/PageObject/LiberisPageObject.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+
+namespace LiberisTests.Specs.PageObject
+{
+    class LiberisPageObject
+    {
+        private const string Url = "https://www.liberis.com";
+
+        private readonly IWebDriver driver;
+
+        public LiberisPageObject(IWebDriver webDriver) => driver = webDriver;
+
+        private IWebElement GetADemoHome => driver.FindElement(By.XPath("//*[@id='site - navigation']/div[1]/a"));
+        private IWebElement GetADemo => driver.FindElement(By.XPath("//*[@id='site - inner - wrapper']/main/section/div[2]/div/div[2]/div/a"));
+        private IWebElement ErrorMessage => driver.FindElement(By.XPath("//*[@id='site - inner - wrapper']/main/section/div[2]/div/div[2]/div/div[2]/div"));
+        
+        public void ClickGetADemoHome()
+        {
+            GetADemoHome.Click();
+        }
+
+        public void ClickGetADemo()
+        {
+            GetADemo.Click();
+        }
+
+        public string GetErrorMessage => ErrorMessage.Text;
+
+        public void OpenBrowserIfClosed()
+        {
+            if (driver.Url != Url)
+            {
+                driver.Url = Url;
+            }
+        }
+
+    }
+}
